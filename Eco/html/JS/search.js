3d2400609592cd2d70a82c8f996a8a16dcce8b70
Lesliezj09 <81850120+Lesliezj09@users.noreply.github.com>
2021-09-11 23:42:31 +0900

    Add files via upload

---
+++ Eco/html/JS/search.js
@@ -0,0 +1,23 @@
+window.onload=function()
+{
+    var btn=document.getElementById('btn');
+    var texts=document.getElementById('texts');
+    var content=document.getElementById('content');
+
+    var str=content.innerHTML;
+
+
+    console.log(str);
+    btn.onclick=function(){
+
+        var val=texts.value;
+        if (val=="")
+        {
+            alert("Search...");
+        }
+        else
+        {
+            content.innerHTML=str.split(val).join('<span id="Stress">'+val+'</span>');
+        }
+    }
+}
